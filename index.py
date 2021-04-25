4064b601a21c0ee3be9d44a7a47e4972142bec2d
Absozero <aswsub123@gmail.com>
2021-04-25 11:15:21 -0700

    Transition to discord: commands exten

---
+++ index.py
@@ -0,0 +1,49 @@
+#data to respond to bot commands goes in this file
+
+#responses for joke command
+jokes = ['What did the traffic light say to the car? Don’t look! I’m about to change.',
+ 'Today at the bank, an old lady asked me to help check her balance. So I pushed her over.', 
+ 'My boss told me to have a good day.. so I went home.',
+ 'I know a lot of jokes about unemployed people but none of them work.',
+ 'Bitter bought some butter but the butter was bitter so bitter bought some more butter to make the bitter butter better. Now say that 3 times real fast.'
+ ]
+
+#responses for me_sad command
+me_sad_ans = ['It\'s okay, it will get better soon.',
+ 'There are people who care for you, you arenever all alone.',
+ 'Well, that\'s why I\'m here to talk with you!',
+ 'Do things that you like to do!'
+ ]
+
+#responses for ur_bad command
+ur_bad_ans = ['Why u bully me?',
+ 'Don\'t you dare talk to me like that',
+ 'I do not accept that mean command :(',
+ 'Bruh :('
+]
+
+#resposes for no u command
+no_u_ans = ['Enter -bruh into chat',
+ 'Are you serious?',
+ 'My own joke against me? That aint finna work.',
+ 'Don\'t even try',
+ 'I shall get revenge for this violation of my own policies!'
+ ]
+
+#responses for wassup command
+wassup_ans = ['Yo, what\'s good?',
+  'Yo',
+  'How\'s life?',
+  'All good here, wbu?',
+  'Doing good, you?'
+  ]
+
+#responses for hi command
+hi_ans = ['Hi!',
+ 'Hello',
+ 'Hey, how are ya?',
+ 'What\'s up?'
+ 'Howdy!'
+]
+
+
