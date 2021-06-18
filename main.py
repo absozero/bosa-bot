115c39366c6263ddd2afad9859628128a92f4e0b
Absozero <aswsub123@gmail.com>
2021-06-18 14:37:08 -0700

    Broken release, do not use

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 
 #Simple code to emulate CLI input and commands
-os.chdir('bot')
-os.system("python bot.py")
+
+if os.path.exists("bot"):
+        os.chdir('bot')
+        os.system("python bot.py")
+else:
+    print('This file is in the wrong place, make sure it is with the other files from the repo.')
 
 #IMPORTANT!
 #The requirements required to run this bot must still be downloaded, they are in requirements.txt
