83384fc6afd4f64c98b07e5e6faed93e4df41c41
Absozero <aswsub123@gmail.com>
2021-06-20 14:20:54 -0700

    Fix items

---
+++ runner.py
@@ -0,0 +1,11 @@
+import os
+
+os.system('git pull https://github.com/absozero/BOSA-bot.git main')
+
+import os
+#Code to run the bot
+
+if os.path.isfile("bot.py"):
+        os.system("python bot.py")
+else:
+    print('This file is in the wrong place, or bot.py file is not available. Make sure it is with the other files from the repo.')
