5398ba32b98e7d24db6018993a59a0bc12a8448c
Absozero <aswsub123@gmail.com>
2021-06-20 13:54:04 -0700

    Fix and move 2 cogs

--- bot/cogs/Greetings.py
+++ bot/cogs/Greetings.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 
+hi_ans = ['Hi!',
+ 'Hello',
+ 'Hey, how are ya?',
+ 'What\'s up?',
+ 'Howdy!'
+]
+
 class Greetings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,6 +15,13 @@ class Greetings(commands.Cog):
     @commands.command()
     async def hello(self, ctx):
         await ctx.send('hi!')
+
+    @commands.command(aliases=['hello', 'wassup', 'whaddup', 'hey', 'yo', 'sup'])
+    async def hi(self, ctx):
+        '''A command to simulate a return to a greeting
+        usage:
+        -[One of the command terms]'''
+        await ctx.send(random.choice(hi_ans) + ' ðŸ‘‹')
 
 def setup(bot):
     bot.add_cog(Greetings(bot))
