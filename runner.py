f7f2ce3e567c21d90ddbe0802e31aae40183fefd
Gitea <gitea@fake.local>
2021-10-03 20:06:28 -0700

    Add linux command

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 import os
+from sys import platform
 
 os.system('git pull https://github.com/absozero/BOSA-bot.git main')
 
@@ -7,5 +8,8 @@ import os
 
 if os.path.isfile("bot.py"):
         os.system("python bot.py")
+if platform == "linux" or platform == "linux2":
+        if os.path.isfile("bot.py"):
+                os.system("python3 bot.py")
 else:
     print('This file is in the wrong place, or bot.py file is not available. Make sure it is with the other files from the repo.')
