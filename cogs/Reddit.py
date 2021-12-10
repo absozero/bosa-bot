120cc8176c8a00017ff3708cad807ebecf8f93b9
Absozero <aswsub123@gmail.com>
2021-12-09 19:27:25 -0800

    As discord.py is deprecated, moving to fork

--- cogs/Reddit.py
+++ cogs/Reddit.py
@@ -2,69 +2,13 @@ import discord
 from discord.ext import commands
 import aiohttp
 import random
-import asyncpraw
 import json
 
-with open("info.json") as file:
-    info = json.load(file)
-
-reddit = asyncpraw.Reddit(client_id = info["reddit_client"],
-client_secret = info["reddit_secret"],
-username = info["reddit_username"],
-password = info["reddit_password"],
-user_agent = info["reddit_useragent"])
 
 class Reddit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def reddit(self, ctx, subred: str, number: int):
-        
-        if 1 <= number <= 35:
-            x = range(number)
-            for i in x:
-                subreddit = await reddit.subreddit(subred)
-                all = []
-
-                hot = subreddit.hot(limit = 13)
-
-                async for submission in hot:
-                    all.append(submission)
-
-                randsub = random.choice(all)
-
-                name = randsub.title
-
-                x = ('ups: {}, downs: {}'.format(randsub.ups, randsub.downs))
-                              
-                url = randsub.url
-                ups = randsub.ups
-                downs = randsub.downs
-                sub = randsub.subreddit
-                auth = randsub.author
-                text = randsub.selftext
-
-                embed = discord.Embed(title=name)
-                embed.set_thumbnail(url=url)
-                embed.set_image(url = url)
-                
-                embed.add_field(name='Upvotes and downvotes', value=f'Up: {ups}, Down: {downs}')
-                embed.add_field(name='reddit', value=url)
-                embed.add_field(name='Subreddit', value=sub)
-
-                embed.set_footer(text=auth)
-
-                await ctx.send(embed = embed)
-                try:
-                    await ctx.send(text)
-                except discord.errors.HTTPException:
-                    await ctx.send('There is no text to send')
-        elif number <= 0:
-            await ctx.send(f'You sent for {number} reddit posts. That wont work because {number} is less than or equal to zero.')
-    
-        else:
-            await ctx.send(f'You asked for {number} reddit posts. That\'s too much, way over the limit of 35! The reason the limit is at 35 is to avoid spamming, but 35 might be too much in itself')
 
     @commands.command()
     async def redit(self, ctx, sub: str, num: int):
